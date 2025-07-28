6a08484c1db9c4681be5973eeca0d6ce0cf5e96a
manishabathala <167901812+manishabathala@users.noreply.github.com>
2025-07-28 09:52:14 +0530

    Update AddNewUser.spec.js

--- tests/AddNewUser.spec.js
+++ tests/AddNewUser.spec.js
@@ -5,12 +5,9 @@ import {login_setup} from '../utility/login_setup'
 import {data} from '../utility/data'
 
 
-test.beforeEach(async({page})=>{
-    await login_setup(page);
-})
-
 test('Add New User',async({page})=>{
     const adminpage=new Orange_AdminPage(page)
+    await login_setup(page)
     await adminpage.addNewUser(data.newUser.role,data.newUser.employeeName,data.newUser.status,data.newUser.username,data.newUser.password)
     await page.waitForURL("https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers")
     await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers")
