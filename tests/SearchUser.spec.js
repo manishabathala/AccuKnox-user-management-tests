81fb512b34865d4714df3b9144a6140cfbf169fc
manishabathala <167901812+manishabathala@users.noreply.github.com>
2025-07-28 09:53:49 +0530

    Update SearchUser.spec.js

--- tests/SearchUser.spec.js
+++ tests/SearchUser.spec.js
@@ -4,12 +4,10 @@ import { Orange_AdminPage } from '../pages/OrangeHRM_AdminPage'
 import {data} from '../utility/data'
 
 
-test.beforeEach(async({page})=>{
-    await login_setup(page);
-})
 
 test('Search User By UserName',async({page})=>{
     const adminpage=new Orange_AdminPage(page)
+    await login_setup(page)
     await adminpage.searchUserByUserName(data.newUser.username)
     await expect(page.locator("//span[normalize-space()='(1) Record Found']")).toBeVisible()
 })
